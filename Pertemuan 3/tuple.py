eaa6795d73d551bc5a1a4c7dbaea177b0982f331
fayydzi <fairuskeduaa@gmail.com>
2026-03-27 18:58:03 +0700

    revisi: perbaikan di tuple.py

--- Pertemuan 3/tuple.py
+++ Pertemuan 3/tuple.py
@@ -8,7 +8,7 @@ print (sys.getsizeof(logApps))
 logAppsList = ["user1 login"]
 print(logAppsList)
 print("memiliki ukurna list", sys.getsizeof(logAppsList))
-print("Lasiah")
+print("Fayy")
 
 #pembuktian Tuple tidak bisa diubaT
 # 1. Tambah 
