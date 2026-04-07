9d17c3c43e12421ed9f26fc411e5cff40df2c0bb
fayydzi <fairuskeduaa@gmail.com>
2026-04-07 10:36:23 +0700

    Pertemuan 4

---
+++ Pertemuan 5/Latihan_dict_dasar.py
@@ -0,0 +1,22 @@
+# 1. Create: Membuat Dictionary (Analogi Database User)
+user_db = {
+    "1001": {"nama": "Alice", "role": "Admin", "status": "Active"},
+    "1002": {"nama": "Bob", "role": "User", "status": "Suspended"}
+}
+
+# 2. Read: Mengakses data menggunakan Key
+print(f"Nama User 1001: {user_db['1001']['nama']}")
+
+# 3. Update: Mengubah data atau menambah Key baru
+user_db["1002"]["status"] = "Active" # Mengubah status Bob
+user_db["1003"] = {"nama": "Charlie", "role": "Guest", "status": "Active"} # Menambah user baru
+
+# 4. Delete: Menghapus data
+del user_db["1003"]
+
+# 5. Iterasi: Mengambil semua data
+print("\n--- Daftar User Aktif ---")
+for uid, info in user_db.items():
+    print(f"ID: {uid} | Nama: {info['nama']} | Role: {info['role']}")
+
+
